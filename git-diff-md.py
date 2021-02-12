5d80d9a7432f6b602f45b339682e877c70f1a673
Phillip Alday <me@phillipalday.com>
2021-02-12 16:22:46 +0100

    rewrite bib extractor, add license to python files

--- git-diff-md.py
+++ git-diff-md.py
@@ -1,10 +1,26 @@
 #! /usr/bin/env python3
+"""
+Copyright (c) 2018, Phillip Alday
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import sys
 import argparse
 from subprocess import call
 
-parser = argparse.ArgumentParser(description='Change boldfance for Pandoc Markdown',
+parser = argparse.ArgumentParser(description='Diff Documents for Pandoc Markdown',
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('revision',type=str,default="0",
